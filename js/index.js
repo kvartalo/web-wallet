afa1bf3f738ab665afb4779e6250110a5ef17f4a
arnaucube <arnaucube@gmail.com>
2019-02-14 19:30:17 +0100

    transactions improved

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
+var web3 = new Web3(new Web3.providers.HttpProvider('https://testnet-rpc.gochain.io'));
+const buf = b => ethUtil.toBuffer(b)
+const sha3 = b => web3.utils.soliditySha3(b)
+const uint256 = n => "0x"+n.toString(16).padStart(64,'0')
+const uint8 = n => "0x"+n.toString(16)
+
+
+
 const RELAYURL = 'http://127.0.0.1:3000';
 const TOKENADDR = '0x9BD08b875A9Bf7Fc6889390bE4704458cD695074';
 
 let myAddr = "";
 let myBalance = 0;
+let myNonce = 0;
+
 console.log("l", localStorage.getItem("myAddr"));
 if (localStorage.getItem("myAddr")===null) {
 	myAddr = newRandKey();
@@ -46,6 +56,7 @@ document.getElementById('myAddrBox').value=myAddr;
 
 function getBalance() {
 	console.log("getting balance");
+	document.getElementById('spinnerBalance').className = 'spinner-border';
 	// show current myAddr balance
 	axios.get(RELAYURL + '/balance/' + myAddr)
 	  .then(function (res) {
@@ -53,32 +64,16 @@ function getBalance() {
 	    console.log(res.data);
 	    console.log("balance " + myBalance);
 	    document.getElementById('myBalanceBox').innerHTML=myBalance;
+	    document.getElementById('spinnerBalance').className += 'invisible';
 	  })
 	  .catch(function (error) {
 	    console.log(error);
 	    toastr.error(error);
+	    document.getElementById('spinnerBalance').className += 'invisible';
 	  });
 }
 
-let myNonce = 0;
-function getTxNonce() {
-	axios.get(RELAYURL + '/tx/nonce/' + myAddr)
-	  .then(function (res) {
-	    myNonce = res.data.nonce;
-	    console.log(res.data);
-	    console.log("myNonce " + myBalance);
-	  })
-	  .catch(function (error) {
-	    console.log(error);
-	    toastr.error(error);
-	  })
-}
 
-var web3 = new Web3(new Web3.providers.HttpProvider('https://testnet-rpc.gochain.io'));
-const buf = b => ethUtil.toBuffer(b)
-const sha3 = b => web3.utils.soliditySha3(b)
-const uint256 = n => "0x"+n.toString(16).padStart(64,'0')
-const uint8 = n => "0x"+n.toString(16)
 function transact() {
 	let toAddr = document.getElementById("toAddr").value;
 	if(toAddr==undefined) {
@@ -98,67 +93,46 @@ function transact() {
 		toastr.error("not valid amount");
 		return;
 	}
-        let msg = "0x" + buf(uint8(0x19)).toString('hex') + buf(uint8(0)).toString('hex') + buf(TOKENADDR).toString('hex') + buf(uint256(myNonce)).toString('hex') + buf(myAddr).toString('hex') + buf(toAddr).toString('hex') + buf(uint256(amount)).toString('hex')
-	let privK = localStorage.getItem(myAddr);
-        let sig = ethUtil.ecsign(buf(sha3(msg)),buf(privK));
-	let txData = {
-		from: myAddr,
-		to: toAddr,
-		value: Number(amount),
-		r: sig.r.toString('hex'),
-		s: sig.s.toString('hex'),
-		v: sig.v
-	};
-	console.log(txData);
-	axios.post(RELAYURL + '/tx', txData)
+	document.getElementById('spinnerTx').className = 'spinner-border';
+	axios.get(RELAYURL + '/tx/nonce/' + myAddr)
 	  .then(function (res) {
-	    console.log(res.data);
-	    toastr.success("transaction created");
-	  })
+		    myNonce = res.data.nonce;
+		    console.log(res.data);
+		    console.log("myNonce " + myNonce);
+		  // after getting nonce, generate & sign & send transaction
+		let msg = "0x" + buf(uint8(0x19)).toString('hex') + buf(uint8(0)).toString('hex') + buf(TOKENADDR).toString('hex') + buf(uint256(myNonce)).toString('hex') + buf(myAddr).toString('hex') + buf(toAddr).toString('hex') + buf(uint256(amount)).toString('hex')
+		let privK = localStorage.getItem(myAddr);
+		let sig = ethUtil.ecsign(buf(sha3(msg)),buf(privK));
+		let txData = {
+			from: myAddr,
+			to: toAddr,
+			value: Number(amount),
+			r: sig.r.toString('hex'),
+			s: sig.s.toString('hex'),
+			v: sig.v
+		};
+		console.log(txData);
+		axios.post(RELAYURL + '/tx', txData)
+		  .then(function (res) {
+		    console.log(res.data);
+		    toastr.success("transaction created");
+			  document.getElementById('spinnerTx').className += 'invisible';
+		  })
+		  .catch(function (error) {
+		    console.log(error);
+		    toastr.error(error);
+		    document.getElementById('spinnerTx').className += 'invisible';
+		  })
+
+	  }) // nonce get error catch
 	  .catch(function (error) {
 	    console.log(error);
 	    toastr.error(error);
+	    document.getElementById('spinnerTx').className += 'invisible';
 	  })
 }
 
 getBalance();
-getTxNonce();
-
-
-// fake data
-let fakeHistoryElems = [
-  {
-    from: 'Shop 1',
-    to: myAddr,
-    value: 10,
-    date: '2019-01-18, 18.40h'
-  },
-  {
-    from: myAddr,
-    to: 'Shop 2',
-    value: 2,
-    date: '2019-01-18, 19.25h'
-  },
-  {
-    from: myAddr,
-    to: 'Shop2',
-    value: 1.5,
-    date: '2019-01-20, 19.25h'
-  },
-  {
-    from: myAddr,
-    to: 'Shop3',
-    value: 1.5,
-    date: '2019-01-20, 19.40h'
-  },
-  {
-    from: myAddr,
-    to: 'Shop1',
-    value: 3,
-    date: '2019-01-20, 21.35h'
-  },
-];
-printHistory(fakeHistoryElems);
 
 
 /*
@@ -166,6 +140,5 @@ TODO (for minimal version):
 - generate mnemonic
 - allow mnemonic backup
 - allow import mnemonic
-- create and sign transactions
 - scan QR with cam
 */
